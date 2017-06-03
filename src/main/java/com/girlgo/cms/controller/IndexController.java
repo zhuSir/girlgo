e7f6cc7be264f966d8da18c00a621a01c77c04c6
大無 <aizgw2012@163.com>
2017-06-03 16:19:11 +0800

    add layui

---
+++ src/main/java/com/girlgo/cms/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.girlgo.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class IndexController {
+
+	@RequestMapping(value="/index")
+	public String index(){
+		
+		return "/index";
+	}
+}
